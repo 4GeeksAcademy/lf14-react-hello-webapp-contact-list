fff79e0f409495439660a1c225f4f0e228383af8
lf14 <54648106+lf14@users.noreply.github.com>
2024-10-27 20:27:06 +0000

    [componentes y views nuevas]

---
+++ src/js/views/AddContact.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const AddContact = () => {
+  return (
+    <div>AddContact
+
+<Link to="/">
+				<span className="btn btn-primary btn-lg" href="#" role="button">
+					Back home
+				</span>
+			</Link>
+    </div>
+  )
+}
+
+export default AddContact
+
---
+++ src/js/views/Agenda.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Agenda = () => {
+  return (
+    <div>Agenda
+        <Link className="btn btn-success" to="/add">
+						Add new contact
+					</Link>
+    </div>
+  )
+}
+
+export default Agenda
